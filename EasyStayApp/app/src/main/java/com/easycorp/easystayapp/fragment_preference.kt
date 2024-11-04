3e1d9784e7239f24e40b9fb5b3ceed06ee0166ec
Aziz Rayene Delaa <adelaa@dti.crosemont.quebec>
2024-11-04 13:15:12 -0500

    Faire internationalization

--- EasyStayApp/app/src/main/java/com/easycorp/easystayapp/fragment_preference.kt
+++ EasyStayApp/app/src/main/java/com/easycorp/easystayapp/fragment_preference.kt
@@ -5,12 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RadioButton
 import android.widget.Switch
 import androidx.appcompat.app.AppCompatDelegate
+import java.util.Locale
 
 class fragment_preference : Fragment() {
 
-    lateinit var switch : Switch
+    lateinit var switch: Switch
+    lateinit var radioFrancais: RadioButton
+    lateinit var radioAnglais: RadioButton
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,10 +25,19 @@ class fragment_preference : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         switch = view.findViewById(R.id.modeSombre)
+        radioFrancais = view.findViewById(R.id.radioFrancais)
+        radioAnglais = view.findViewById(R.id.radioAnglais)
+
         if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES) {
             switch.isChecked = true
         }
+
+        val currentLocale = Locale.getDefault().language
+        radioFrancais.isChecked = currentLocale == "fr"
+        radioAnglais.isChecked = currentLocale == "en"
+
         switch.setOnCheckedChangeListener { _, isChecked ->
             if (isChecked) {
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
@@ -32,5 +45,28 @@ class fragment_preference : Fragment() {
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
             }
         }
+
+        radioFrancais.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                setLocale(Locale("fr"))
+                radioAnglais.isChecked = false
+            }
+        }
+
+        // Listener for English RadioButton
+        radioAnglais.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                setLocale(Locale("en"))
+                radioFrancais.isChecked = false
+            }
+        }
+    }
+
+    private fun setLocale(locale: Locale) {
+        Locale.setDefault(locale)
+        val config = resources.configuration
+        config.setLocale(locale)
+        requireActivity().resources.updateConfiguration(config, requireActivity().resources.displayMetrics)
+        requireActivity().recreate()
     }
 }
