74e864ec2aadaf462a35fade4e61e29590f31579
Ismail El Asraoui <2253910@crosemont.qc.ca>
2024-11-14 16:15:21 -0500

    Presentateur preference ajoute

---
+++ EasyStayApp/app/src/main/java/com/easycorp/easystayapp/Presentation/Presentateur/PreferencePresentateur.kt
@@ -0,0 +1,24 @@
+package com.easycorp.easystayapp.Presentation.Presentateur
+
+import com.easycorp.easystayapp.Presentation.Modele.Modèle
+import com.easycorp.easystayapp.Presentation.Vue.PreferenceVue
+import com.easycorp.easystayapp.R
+import com.easycorp.easystayapp.Domaine.Entite.ClientData
+import com.easycorp.easystayapp.Presentation.Vue.PreferencePresentateurInterface
+
+
+class PreferencePresentateur(
+    val vue: PreferencePresentateurInterface
+) {
+    val modèle = Modèle()
+
+    fun afficherClient(clientId: Int) {
+        val client = modèle.obtenirClientParId(clientId)
+        vue.afficherClient(
+            prénom = client.prénom,
+            nom = client.nom,
+            email = client.email,
+            photoResId = client.photo ?: R.drawable.profil_icon
+        )
+    }
+}
